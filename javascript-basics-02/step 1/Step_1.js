653ee44c660dc7082d31c6e96024c915cac282ad
Sabah <sabahalmel7@gmail.com>
2023-08-30 21:10:56 +0300

    completed Step_1

---
+++ javascript-basics-02/step 1/Step_1.js
@@ -0,0 +1,4 @@
+document.querySelector("#image1").onmouseover=function(){
+    // document.querySelector("#image1").setAttribute("src","images/image1_2.jpg");
+    document.querySelector("#image1").src="images/image1_2.jpg"
+}
